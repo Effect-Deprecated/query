7f6b803a9f2a59c92dc2d42266416a34ba2efedb
Michael Arnaldi <arnaldimichael@gmail.com>
2021-01-12 23:42:31 +0000

    a bit of safety doesn't hurt

--- src/internal/BlockedRequests.ts
+++ src/internal/BlockedRequests.ts
@@ -1,4 +1,5 @@
 // port of: https://github.com/zio/zio-query/blob/3f9f4237ca2d879b629163f23fe79045eb29f0b0/zio-query/shared/src/main/scala/zio/query/internal/BlockedRequests.scala
+import * as S from "@effect-ts/core/Sync";
 import * as DS from "src/DataSource";
 import { DataSourceAspect } from "src/DataSourceAspect";
 import { BlockedRequest } from "./BlockedRequest";
@@ -62,10 +63,9 @@ export function both<R>(
  * Combines this collection of blocked requests with the specified collection
  * of blocked requests, in sequence.
  */
-export function then<R>(
-  fb: BlockedRequests<R>
-): <R1>(fa: BlockedRequests<R1>) => BlockedRequests<R & R1> {
-  return (fa) => new Then(fa, fb as any); // TODO: SHAME!
+export function then<R>(fb: BlockedRequests<R>) {
+  return <R1>(fa: BlockedRequests<R1>): BlockedRequests<R & R1> =>
+    new Then<R & R1>(fa, fb);
 }
 
 /**
@@ -76,33 +76,44 @@ export function then<R>(
 export function mapDataSources<R, R1>(
   f: DataSourceAspect<R, R1>
 ): (fa: BlockedRequests<R>) => BlockedRequests<R1> {
-  // TODO: Stack safety
-  return (fa) => {
-    switch (fa._tag) {
-      case "Empty":
-        return new Empty();
-      case "Both":
-        return new Both(
-          mapDataSources(f)(fa.left),
-          mapDataSources(f)(fa.right)
-        );
-      case "Then":
-        return new Then(
-          mapDataSources(f)(fa.left),
-          mapDataSources(f)(fa.right)
-        );
-      case "Single":
-        return fa.f(
-          (_) =>
-            new Single(($) =>
-              $({
-                dataSource: f(_.dataSource),
-                blockedRequest: _.blockedRequest,
-              })
-            )
-        );
-    }
-  };
+  return (fa) => S.run(mapDataSourcesSafe(f)(fa));
+}
+
+/**
+ * Transforms all data sources with the specified data source aspect, which
+ * can change the environment type of data sources but must preserve the
+ * request type of each data source.
+ */
+export function mapDataSourcesSafe<R, R1>(
+  f: DataSourceAspect<R, R1>
+): (fa: BlockedRequests<R>) => S.UIO<BlockedRequests<R1>> {
+  return (fa) =>
+    S.gen(function* (_) {
+      switch (fa._tag) {
+        case "Empty":
+          return new Empty();
+        case "Both":
+          return new Both(
+            yield* _(mapDataSourcesSafe(f)(fa.left)),
+            yield* _(mapDataSourcesSafe(f)(fa.right))
+          );
+        case "Then":
+          return new Then(
+            yield* _(mapDataSourcesSafe(f)(fa.left)),
+            yield* _(mapDataSourcesSafe(f)(fa.right))
+          );
+        case "Single":
+          return fa.f(
+            (_) =>
+              new Single(($) =>
+                $({
+                  dataSource: f(_.dataSource),
+                  blockedRequest: _.blockedRequest,
+                })
+              )
+          );
+      }
+    });
 }
 
 /**
@@ -112,33 +123,43 @@ export function provideSome<R, R0>(
   description: string,
   f: (a: R0) => R
 ): (fa: BlockedRequests<R>) => BlockedRequests<R0> {
-  // TODO: Stack safety
-  return (fa) => {
-    switch (fa._tag) {
-      case "Empty":
-        return new Empty();
-      case "Both":
-        return new Both(
-          provideSome(description, f)(fa.left),
-          provideSome(description, f)(fa.right)
-        );
-      case "Then":
-        return new Then(
-          provideSome(description, f)(fa.left),
-          provideSome(description, f)(fa.right)
-        );
-      case "Single":
-        return fa.f(
-          (_) =>
-            new Single(($) =>
-              $({
-                dataSource: DS.provideSome(description, f)(_.dataSource),
-                blockedRequest: _.blockedRequest,
-              })
-            )
-        );
-    }
-  };
+  return (fa) => S.run(provideSomeSafe(description, f)(fa));
+}
+
+/**
+ * Provides each data source with part of its required environment.
+ */
+export function provideSomeSafe<R, R0>(
+  description: string,
+  f: (a: R0) => R
+): (fa: BlockedRequests<R>) => S.UIO<BlockedRequests<R0>> {
+  return (fa) =>
+    S.gen(function* (_) {
+      switch (fa._tag) {
+        case "Empty":
+          return new Empty();
+        case "Both":
+          return new Both(
+            yield* _(provideSomeSafe(description, f)(fa.left)),
+            yield* _(provideSomeSafe(description, f)(fa.right))
+          );
+        case "Then":
+          return new Then(
+            yield* _(provideSomeSafe(description, f)(fa.left)),
+            yield* _(provideSomeSafe(description, f)(fa.right))
+          );
+        case "Single":
+          return fa.f(
+            (_) =>
+              new Single(($) =>
+                $({
+                  dataSource: DS.provideSome(description, f)(_.dataSource),
+                  blockedRequest: _.blockedRequest,
+                })
+              )
+          );
+      }
+    });
 }
 
 /**
